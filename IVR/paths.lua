0f628e7b9e252ff43ab7bc314c7090fceade5fc7
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2009-12-03 13:31:27 +0000

    seperated out pathnames, added record response at end of responses, fixed bugs in mesage creation to do with defaultin lft/rgt, creating new users

---
+++ IVR/paths.lua
@@ -0,0 +1,5 @@
+logfile = io.open("/home/dsc/Documents/Log/AO/ao.log", "a");
+basedir = "/usr/local/freeswitch";
+bsd = basedir .. "/sounds/en/us/callie/";
+aosd = basedir .. "/scripts/AO/sounds/eng/";
+sd = "/home/dsc/Development/audio/";
