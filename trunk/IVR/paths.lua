20c073eb6a3371a13529fa3fde40a3546c974832
tparikh <tparikh@1acfb158-7363-11de-834f-c7280bcb9d8a>
2009-12-12 04:16:16 +0000

    Refactored the playmessages code, fixed a couple bugs and some other mucking about.  We should talk about my comments.

--- trunk/IVR/paths.lua
+++ trunk/IVR/paths.lua
@@ -1,8 +1,11 @@
-logfile = io.open("/home/dsc/Documents/Log/AO/ao.log", "a");
 basedir = "/usr/local/freeswitch";
 bsd = basedir .. "/sounds/en/us/callie/";
+logfilename = "/home/dsc/Documents/Log/AO/ao.log";
 aosd = basedir .. "/scripts/AO/sounds/guj/";
 sd = "/home/dsc/Development/audio/";
+--logfilename = "/Library/WebServer/Documents/AO/ao.log";
+--aosd = basedir .. "/scripts/AO/sounds/eng/";
+--sd = basedir .. "/storage/otalo/";
 
 -- GLOBALS
 env = assert (luasql.mysql());
@@ -11,6 +14,7 @@ con = assert (env:connect("otalo","otalo","otalo","localhost"));
 -- ADMINS
 cur = con:execute("SELECT number from AO_user WHERE admin = 'y'" );
 local admins = cur:fetch({});
+cur:close();
 
 function adminmode()
 	local caller = session:getVariable("caller_id_number");
