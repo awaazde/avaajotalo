34a3933340970bb48e556f5cc5dddc51e682db31
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2010-01-05 23:56:39 +0000

    logout implemented

--- web/django/settings.py
+++ web/django/settings.py
@@ -70,7 +70,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/neil/Development/html/django_templates',
+    '/home/neil/Development/workspace/django_templates',
     '/home/neil/Development/workspace/ao/war',
 )
 
