9dea7a3dc87120d17d1430deaf9807baa64fbeea
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2010-04-27 21:49:25 +0000

    Add license header

--- trunk/web/django/settings.py
+++ trunk/web/django/settings.py
@@ -1,3 +1,19 @@
+#===============================================================================
+#    Copyright (c) 2009 Regents of the University of California, Stanford University, and others
+# 
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+# 
+#        http://www.apache.org/licenses/LICENSE-2.0
+# 
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#===============================================================================
+
 # Django settings for otalo project.
 
 DEBUG = True
@@ -36,7 +52,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/neil/Development/audio'
+MEDIA_ROOT = '/Users/neil/Development/audio'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -70,8 +86,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/neil/Development/workspace/django_templates',
-    '/home/neil/Development/workspace/ao/war',
+    '/Users/neil/Development/workspace/django_templates',
+    '/Users/neil/Development/workspace/ao/war',
 )
 
 INSTALLED_APPS = (
@@ -83,7 +99,7 @@ INSTALLED_APPS = (
     'django.contrib.admin'
 )
 
-STATIC_DOCUMENT_ROOT = '/home/neil/Development/workspace/ao/war'
+STATIC_DOCUMENT_ROOT = '/Users/neil/Development/workspace/ao/war'
 
 SERIALIZATION_MODULES = {
     'json': 'wadofstuff.django.serializers.json'
