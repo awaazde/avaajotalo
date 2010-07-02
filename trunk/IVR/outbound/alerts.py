387cdc4c95c28242f0d64404dee3e8614d530033
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2010-07-02 11:01:04 +0000

    bug fixes and tuning for missed calls on production

--- trunk/IVR/outbound/alerts.py
+++ trunk/IVR/outbound/alerts.py
@@ -20,69 +20,66 @@ from ESL import *
 import re, time, sched
 from threading import Timer
 
-INTERVAL_HOURS = 12
+# should match the interval of the cron running this script
+INTERVAL_HOURS = 1
 IVR_SCRIPT = 'AO/outbound/missed_call.lua'
 MAX_WAIT_SECS = 12
 EXEC_WAIT_SECS = 7
-# since it's just a missed call, it doesn't matter which
-# line we use (there is no charge)
-line = Line.objects.get(pk=1)
 
-def new_responses():
-     # Get all new responses in the last INTERVAL_HOURS
-     interval = timedelta(hours=INTERVAL_HOURS)
-     now = datetime.now()
+def new_responses(line, user_ids=False):
+     if not user_ids:
+     	# Get all new responses in the last INTERVAL_HOURS
+     	interval = timedelta(hours=INTERVAL_HOURS)
+     	now = datetime.now()
      
-     # get messages with responses in the last INTERVAL_HOURS
-     new_response_thread_ids = Message.objects.filter(lft__gt=1, date__gte=now-interval).values_list('thread', flat=True).distinct()
-     # get the authors of the creators of the threads
-     user_ids = User.objects.filter(message__id__in=new_response_thread_ids).values_list('id', flat=True).distinct()
-     user_ids = [2] 
-     #router.route_calls(user_ids, IVR_SCRIPT)
-     missed_call(user_ids)
+    	# Get messages with responses in the last INTERVAL_HOURS
+     	new_response_thread_ids = Message.objects.filter(lft__gt=1, date__gte=now-interval, message_forum__forum__line=line).values_list('thread', flat=True).distinct()
+     	# Get the authors of the creators of the threads
+     	user_ids = User.objects.filter(message__id__in=new_response_thread_ids).values_list('id', flat=True).distinct()
+     
+     missed_call(line, user_ids)
 
 def hangup(uid):
 	# hangup call
-	print("hupall normal_clearing uid " + str(uid) + ": hangup command")
+	#print("hupall normal_clearing uid " + str(uid) + ": hangup command")
     	con = ESLconnection('127.0.0.1', '8021', 'ClueCon')
 	con.api('hupall normal_clearing uid ' + str(uid))
+	con.disconnect()
 	     
-def missed_call(user_ids):
-    con = ESLconnection('127.0.0.1', '8021', 'ClueCon')
- 
-    if not con.connected():
-        print 'Not Connected'
-        sys.exit(2)
-
-    con.events("plain", "all");
+def missed_call(line, user_ids):
     for uid in user_ids:
+    	con = ESLconnection('127.0.0.1', '8021', 'ClueCon')
+    	con.events("plain", "all");
         user = User.objects.get(pk=uid)
-        print('user.num= ' + user.number)
         con.api("originate {uid=" + str(uid) + "}" + str(line.dialstring_prefix) + str(user.number) +  str(line.dialstring_suffix) + " &echo")
-        print(str(user.number) + ": originated call")
+        #print(str(user.number) + ": originated call")
         # either wait for the right event or timeout, whichever comes first
         Timer(MAX_WAIT_SECS, hangup, [uid]).start()
-    while 1:
-        e = con.recvEvent() 
-        if e:
-            type = e.getType()
-            print ('Type ' + type)
-            if type == 'CHANNEL_STATE':
-            	body = e.serialize()
-            	m = re.search("Channel-State: CS_(\w+)", body)
-            	if m:
-                	state = m.group(1)
-                	print("CHANNEL state: " + state)
-            	if state == 'EXECUTE':
-        		Timer(EXEC_WAIT_SECS, hangup, [uid]).start()
-		if state == 'DESTROY':
-            		hangup(uid)
-            		break 						
+    	while 1:
+        	e = con.recvEvent() 
+        	if e:
+            		type = e.getType()
+            		#print ('Type ' + type)
+            		if type == 'CHANNEL_STATE':
+            			body = e.serialize()
+            			m = re.search("Channel-State: CS_(\w+)", body)
+            			if m:
+                			state = m.group(1)
+                			#print("CHANNEL state: " + state)
+            				if state == 'EXECUTE':
+        					Timer(EXEC_WAIT_SECS, hangup, [uid]).start()
+					if state == 'DESTROY':
+            					hangup(uid)
+            					break 						
+	con.disconnect()
+	
 def main():
-    if len(sys.argv) == 2:
-        responder_id = sys.argv[1]
-        missed_call([responder_id])
-    else:
-        new_responses()
-
+    if len(sys.argv) > 1:
+	line = sys.argv[1]
+	line = Line.objects.get(pk=(int(line)))
+	if len(sys.argv) > 2:
+        	user_ids = sys.argv[2]
+        	new_responses(line, user_ids.split(','))
+	else:
+		new_responses(line)
 main()
