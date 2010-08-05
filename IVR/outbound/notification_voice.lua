95c5eda9acd535c1f33df4984e7e80c9963ec363
agathe.battestini <agathe.battestini@1acfb158-7363-11de-834f-c7280bcb9d8a>
2010-08-05 00:27:52 +0000

    fixed the lua code and added the sms sent to SMS gateway directly. lua code tested for US phone numberwq

--- IVR/outbound/notification_voice.lua
+++ IVR/outbound/notification_voice.lua
@@ -25,16 +25,16 @@ dofile("/usr/local/freeswitch/scripts/AO/paths.lua");
 dofile("/usr/local/freeswitch/scripts/AO/common.lua");
 
 -- overwrite standard logfile
-logfilename = "/home/neil/Log/AO/notification_voice.log";
+logfilename = "/home/nokia/notification_voice.log";
 logfile = io.open(logfilename, "a");
 logfile:setvbuf("line");
 
 script_name = "notification_voice.lua";
-aosd = basedir .. "/scripts/AO/sounds/";
+aosd = basedir .. "/sounds/notifications/";
 -- script-specific sounds XXXX TO DO - where are the tag files
-tagsd = aosd .. "tags/";
-notsd = aosd .. "notification/";
-CALLID_VAR = '{ao_notification=true}';
+tagsd = aosd .. "";
+notsd = aosd .. "";
+CALLID_VAR = '{ao_responder=true,ignore_early_media=true}';
 
 digits = "";
 arg = {};
@@ -51,6 +51,7 @@ notification_message_id = argv[1];
 query = 		"SELECT  number, voice_message_tag ";
 query = query .. " FROM notification_notificationmessage";
 query = query .. " WHERE id = " .. notification_message_id;
+logfile:write("info", script_name .. " : query : " .. query .. "\n");
 freeswitch.consoleLog("info", script_name .. " : query : " .. query .. "\n");
 res = row(query);
 phonenum = res[1];
@@ -60,51 +61,57 @@ tag = res[2];
 query = 		"SELECT  tag_file ";
 query = query .. " FROM AO_tag";
 query = query .. " WHERE tag = '" .. tag .. "'";
+logfile:write("info", script_name .. " : query : " .. query .. "\n");
 freeswitch.consoleLog("info", script_name .. " : query : " .. query .. "\n");
 res = row(query);
 tag_file = res[1];
 
 
-DIALSTRING_PREFIX = "";
+DIALSTRING_PREFIX = "sofia/gateway/gizmo/";
 DIALSTRING_SUFFIX = "";
 
---------------------------------------------------------------------------------
--- set_voice_notification
---------------------------------------------------------------------------------
-function set_voice_notification (notid)
-   local query = " UPDATE notification_notificationmessage ";
-   -- XXXX TODO replace 111111 by the current time
-   -- query = query .. " SET sent_on = 11111111 ";
-   query = query .. " SET sent_on = created_on";
-   query = query .. " WHERE id = " .. notid;
-   con:execute(query);
-   freeswitch.consoleLog("info", script_name .. " : query : " .. query .. "\n");
-end
 
 --------------------------------------------------------------------------------
 -- MAIN 
 --------------------------------------------------------------------------------
 
 -- make the call
-session = freeswitch.Session(CALLID_VAR .. DIALSTRING_PREFIX .. phonenum .. DIALSTRING_SUFFIX)
-session:setVariable("caller_id_number", phonenum)
+logfile:write("CALLING " .. CALLID_VAR .. DIALSTRING_PREFIX .. phonenum .. DIALSTRING_SUFFIX .. "\n");
+session = freeswitch.Session(CALLID_VAR .. DIALSTRING_PREFIX .. phonenum .. DIALSTRING_SUFFIX);
+
+session:setVariable("caller_id_number", phonenum);
 session:setVariable("playback_terminators", "#");
 session:setHangupHook("hangup");
+
 
 if (session:ready() == true) then
 	-- sleep for a bit
 	session:sleep(10000);
-	logfile:write(sessid, "\t", session:getVariable("caller_id_number"), "\t", session:getVariable("destination_number"),
+	logfile:write("MAKING CALL sessid=" .. sessid, "\t", session:getVariable("caller_id_number"), "\t", session:getVariable("destination_number"),
 	"\t", os.time(), "\t", "Start call", "\n");
 	
-	-- XXXX TO DO use the file that has: you have new messages in your inbox about
-	read(notsd .. "youhavemessages.wav", 500);
+	logfile:write("READ  " .. notsd .. "youhavemessages_pre.wav" .. "\n");
+	read(notsd .. "youhavemessages_pre.wav", 500);
 	-- XXXX TODO use the file that has for instance: millet.wav (one for each tag)
-	read(tagsd .. tag ..".wav", 500);
+	read(tagsd .. tag_file, 1000);
+	--read(tagsd .. "cotton.wav", 1000); -- example
+	read(notsd .. "youhavemessages_post.wav", 1500);
 
-	-- DONE, update the DB
-	
+	-- update the DB
+	logfile:write("update DB  " .. "\n");
+	query = " UPDATE notification_notificationmessage ";
+	query = query .. " SET sent_on = now()";
+	query = query .. " WHERE id = " .. notification_message_id;
+	logfile:write("info", script_name .. " : query : " .. query .. "\n");
+	-- con:execute(query);
+	res = row(query);
+
+	hangup();
 end
+
+
+
+
 
 
 
