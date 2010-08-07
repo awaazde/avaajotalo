4482f6870d8352e9f479ec89fff853ccc3111fa0
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2010-08-07 14:30:38 +0000

    bug fix answer_call

--- IVR/outbound/alerts.py
+++ IVR/outbound/alerts.py
@@ -15,11 +15,19 @@
 #===============================================================================
 import sys,router
 from datetime import datetime, timedelta
-from otalo.AO.models import Message, User, Line, User
+from otalo.AO.models import Message, Message_forum, User, Line, User
+from otalo.surveys.models import Subject, Survey, Prompt, Option, Call
 from otalo.settings import MEDIA_ROOT
 from ESL import *
 import re, time, sched
 from threading import Timer
+
+# These should be consistent with the constants
+# in survey.lua
+OPTION_NEXT = 1
+OPTION_PREV = 2
+OPTION_REPLAY = 3
+OPTION_GOTO = 4
 
 # should match the interval of the cron running this script
 INTERVAL_HOURS = 1
@@ -52,6 +60,7 @@ def missed_call(line, user_ids):
     	con = ESLconnection('127.0.0.1', '8021', 'ClueCon')
     	con.events("plain", "all");
         user = User.objects.get(pk=uid)
+	#print("originate {uid=" + str(uid) + "}" + str(line.dialstring_prefix) + str(user.number) +  str(line.dialstring_suffix) + " &echo")
         con.api("originate {uid=" + str(uid) + "}" + str(line.dialstring_prefix) + str(user.number) +  str(line.dialstring_suffix) + " &echo")
         #print(str(user.number) + ": originated call")
         # either wait for the right event or timeout, whichever comes first
@@ -74,11 +83,17 @@ def missed_call(line, user_ids):
             					break 						
 	con.disconnect()
 
-def answer_call(answer, userid):
+def answer_call(line, userid, answer):
     prefix = line.dialstring_prefix
     suffix = line.dialstring_suffix
     
-    asker = User.objects.get(pk=userid)
+    user = User.objects.get(pk=userid)
+    asker = Subject.objects.filter(pk=user.number)
+    if not bool(asker):
+	asker = Subject(name=user.name, number=user.number)
+	asker.save()
+    else:
+	asker = asker[0]
     now = datetime.now()
     
     s = Survey(name="AnswerCall_" + str(asker), dialstring_prefix=prefix, dialstring_suffix=suffix, complete_after=2)
@@ -86,19 +101,19 @@ def answer_call(answer, userid):
     s.save()
 
     # welcome
-    welcome = Prompt(file="welcome_answercall.mp3", order=1, bargein=False, survey=s)
+    welcome = Prompt(file=line.language+"/welcome_answercall.mp3", order=1, bargein=False, survey=s)
     welcome.save()
     welcome_opt = Option(number="", action=OPTION_NEXT, prompt=welcome)
     welcome_opt.save()
     
 
-    a = Prompt(file=MEDIA_ROOT+answer.message.content_file, order=2, bargein=False, survey=s)
+    a = Prompt(file=MEDIA_ROOT+'/'+answer.message.content_file, order=2, bargein=False, survey=s)
     a.save()
     a_opt = Option(number="", action=OPTION_NEXT, prompt=a)
     a_opt.save()
     
     # thanks
-    thanks = Prompt(file="thankyou_answercall.mp3", order=3, bargein=False, survey=s)
+    thanks = Prompt(file=line.language+"/thankyou_answercall.mp3", order=3, bargein=False, survey=s)
     thanks.save()
     thanks_opt = Option(number="", action=OPTION_NEXT, prompt=thanks)
     thanks_opt.save()
@@ -125,10 +140,17 @@ def answer_call(answer, userid):
 def main():
     if len(sys.argv) > 1:
 	line = sys.argv[1]
-	line = Line.objects.get(pk=(int(line)))
+	line = Line.objects.get(pk=int(line))
 	if len(sys.argv) > 2:
-        	user_ids = sys.argv[2]
-        	new_responses(line, user_ids.split(','))
+		if len(sys.argv) > 3:
+			user_id = sys.argv[2]
+			mfid = sys.argv[3]
+			mf = Message_forum.objects.get(pk=int(mfid))
+			answer_call(line,int(user_id),mf)
+		else:
+			user_ids = sys.argv[2].split(',')
+        		#new_responses(line, user_ids.split(','))
+			missed_call(line, user_ids)
 	else:
 		new_responses(line)
 #main()
