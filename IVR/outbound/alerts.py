ddf9ba81c6ca5a49425c2b3b845ea303351637be
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2011-01-01 11:00:31 +0000

    bug fix: on answer call alerts, set the origination number to the appropriate line

--- IVR/outbound/alerts.py
+++ IVR/outbound/alerts.py
@@ -97,7 +97,7 @@ def answer_call(line, userid, answer):
         
     now = datetime.now()
     
-    s = Survey(name="AnswerCall_" + str(asker), dialstring_prefix=prefix, dialstring_suffix=suffix, complete_after=1)
+    s = Survey(name="AnswerCall_" + str(asker), dialstring_prefix=prefix, dialstring_suffix=suffix, complete_after=1, number=line.number)
     #print ("adding announcement survey " + str(s))
     s.save()
 
