e5e84c992cc4b09cf32ac51478edf570102cb261
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2011-02-22 15:54:03 +0000

    template creator

---
+++ web/django/surveys/standard_templates.py
@@ -0,0 +1,93 @@
+#===============================================================================
+#    Copyright (c) 2009 Regents of the University of California, Stanford University, and others
+# 
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+# 
+#        http://www.apache.org/licenses/LICENSE-2.0
+# 
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#===============================================================================
+import sys
+from datetime import datetime, timedelta
+from otalo.AO.models import Line
+from otalo.surveys.models import Survey, Subject, Call, Prompt, Option
+from random import shuffle
+import broadcast
+
+# These should be consistent with the constants
+# in survey.lua
+OPTION_NEXT = 1
+OPTION_PREV = 2
+OPTION_REPLAY = 3
+OPTION_GOTO = 4
+OPTION_RECORD = 5
+OPTION_INPUT = 6
+OPTION_TRANSFER = 7
+
+SOUND_EXT = ".wav"
+
+def standard_template(line, contenttype):
+    prefix = line.dialstring_prefix
+    suffix = line.dialstring_suffix
+    language = line.language
+    if line.outbound_number:
+        num = line.outbound_number
+    else:
+        num = line.number
+    
+    name = contenttype[:3].upper() + '_' + broadcast.TEMPLATE_DESIGNATOR + ' (' + str(line.id) + ')'
+    
+    s = Survey.objects.filter(name=name)
+    if not bool(s):
+        s = Survey(name=name, dialstring_prefix=prefix, dialstring_suffix=suffix, complete_after=0, number=num, template=True)
+        print ("adding template " + str(s))
+        s.save()
+    
+        # welcome
+        welcome = Prompt(file=language+"/welcome"+SOUND_EXT, order=1, bargein=True, survey=s)
+        welcome.save()
+        welcome_opt1 = Option(number="", action=OPTION_NEXT, prompt=welcome)
+        welcome_opt1.save()
+        welcome_opt2 = Option(number="1", action=OPTION_NEXT, prompt=welcome)
+        welcome_opt2.save()
+        
+        # content
+        content = Prompt(file=language+"/"+contenttype+SOUND_EXT, order=2, bargein=True, survey=s)
+        content.save()
+        content_opt = Option(number="", action=OPTION_NEXT, prompt=content)
+        content_opt.save()
+        content_opt2 = Option(number="1", action=OPTION_NEXT, prompt=content)
+        content_opt2.save()
+        
+        # thanks
+        thanks = Prompt(file=language+"/thankyou"+SOUND_EXT, order=4, bargein=True, delay=5000, survey=s)
+        thanks.save()
+        thanks_opt1 = Option(number="", action=OPTION_NEXT, prompt=thanks)
+        thanks_opt1.save()
+        thanks_opt2 = Option(number="1", action=OPTION_NEXT, prompt=thanks)
+        thanks_opt2.save()
+        
+        return s
+    else:
+        return s[0] 
+ 
+def main():
+    # get the forum
+    if len(sys.argv) < 2:
+        print("args: lineid")
+        sys.exit()
+    else:
+        lineid = sys.argv[1]
+        
+    line = Line.objects.get(pk=int(lineid))
+    standard_template(line, 'qna')
+    standard_template(line, 'announcement')
+    
+main()
+
