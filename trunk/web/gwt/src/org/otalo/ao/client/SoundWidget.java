e1fb8244d848be474e849be0ffd476ad7b89af7f
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2011-02-26 15:14:28 +0000

    bug fix click on movie in Mozilla. Looks like Firefox treats differently the widgets in tables based on the order they are added.

--- trunk/web/gwt/src/org/otalo/ao/client/SoundWidget.java
+++ trunk/web/gwt/src/org/otalo/ao/client/SoundWidget.java
@@ -21,26 +21,17 @@ public class SoundWidget {
 
 	private HTML sound;
 	
-	public SoundWidget()
+	public SoundWidget(String filename)
 	{
 		sound = new HTML("<object width=\"200\" height=\"24\" type=\"application/x-shockwave-flash\" data=\"ao/swf/player.swf\" id=\"audioplayer2\">"  +
-		    "<param name=\"movie\" value=\"ao/swf/player.swf\" />" +
-		    "<param name=\"FlashVars\" value=\"playerID=2&amp;soundFile=dummy.mp3\"  />" +
-		    "<param name=\"quality\" value=\"high\" />" +
-		    "<param name=\"menu\" value=\"false\" />" +
-		    "<param name=\"wmode\" value=\"transparent\" /> </object>");
-		
-	}
-	
-	public void setFilename(String filename)
-	{
-		sound.setHTML("<object width=\"200\" height=\"24\" type=\"application/x-shockwave-flash\" data=\"ao/swf/player.swf\" id=\"audioplayer2\">"  +
     "<param name=\"movie\" value=\"ao/swf/player.swf\" />" +
     "<param name=\"FlashVars\" value=\"playerID=2&amp;soundFile=" + filename +"\"  />" +
     "<param name=\"quality\" value=\"high\" />" +
     "<param name=\"menu\" value=\"false\" />" +
     "<param name=\"wmode\" value=\"transparent\" /> </object>");
+		
 	}
+	
 	public HTML getWidget()
 	{
 		return sound;
