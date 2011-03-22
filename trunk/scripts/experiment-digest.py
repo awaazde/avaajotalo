1b79ee4c7edf519ed97110f73b006cdfe86007f8
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2011-03-22 03:32:45 +0000

    new experiment-digest

--- trunk/scripts/experiment-digest.py
+++ trunk/scripts/experiment-digest.py
@@ -1,69 +1,240 @@
-import sys
+import sys, os
 from datetime import datetime, timedelta
-import otalo_utils, survey_stats
-from otalo.surveys.models import Subject, Survey, Call
+from django.db.models import Min, Max
+import otalo_utils, num_calls
+from otalo.surveys.models import Subject, Survey, Prompt, Call, Input
+from otalo.AO.models import Line, Message_forum
 
-def print_digest(f, date):
+STUDY_START = datetime(year=2011, month=3, day=10)
+blacklist_nums = ['9596550654', '9173911854', '9726537942', '7940086740', '9893966806', '7554078142', '9755195845']
+blacklist = Subject.objects.filter(number__in=blacklist_nums)
+
+def print_digest(inbound_log, bang, motiv=None):
+	now = datetime.now()
+	today = datetime(year=now.year, month=now.month, day=now.day)
 	oneday = timedelta(days=1)
+	thisweek = now - timedelta(days=today.weekday())
+	oneweek = timedelta(days=6)
 	
 	print("<html>")
-	print("<div> Below is the experiment report for " + datetime.strftime(date,'%m-%d-%Y') + " </div>")
-	# num subjects
-	nsubjects = Call.objects.filter(date__gte=date).values('subject').distinct().count()
-	print("<br/><div>")
-	print("<b>Number of subjects:</b> ")
-	print(nsubjects)
-	print("</div>")
+	print("<div><h2>This week's experiment results</h3></div>")
+	print("<div><h3>Experiment 1: Free Access vs. Free Contribution </h4></div>")
+	print_bcast_table(bang, ['CALL', 'REC', 'RATE'])
 	
-	# num connected calls
-	ncomplete = Call.objects.filter(date__gte=date, complete=1).count()
-	print("<br/><div>")
-	print("<b>Number of calls completed:</b> ")
-	print(ncomplete)
-	print("</div>")
+	thisweeks_bcasts = Survey.objects.filter(broadcast=True, number__in=[bang.number, bang.outbound_number], call__date__gt=thisweek, call__date__lt=today+oneday).distinct()
+	bcast_prompts = Prompt.objects.filter(survey__in=thisweeks_bcasts, order=3)
+	bcast_prompt_files = [os.path.basename(pair.values()[0]) for pair in bcast_prompts.values('file')]
+	unique_bcasts = Message_forum.objects.filter(message__content_file__in=bcast_prompt_files)
+	all_bcasts = Survey.objects.filter(broadcast=True, number__in=[bang.number, bang.outbound_number], call__date__gt=STUDY_START, call__date__lt=today+oneday).distinct()
 	
-	# num followups
-	print("<div><h4>Followups by info source (with phone numbers)</h4></div>")
+	print("<div><h4>Rating Results</h4></div>")
 	print("<table>")
-
-	#followups = survey_stats.get_followups(filename=f, date_start=date, date_end=date+oneday, quiet=True)
-	followups = survey_stats.get_followups(filename=f, date_start=date, date_end=date+oneday, quiet=True, prompt_filter='solution')
-	for source, calllst in followups.items():
-		print("<tr>")
-		print("<td width='40px'>"+str(source)+"</td>")
-		print("<td>"+str(len(calllst))+"</td>")
-		print("<td>"+str(calllst)+"</td>")
-		print("</tr>")
+	print("<tr>")
+	exp1_header = "<td width='100px'><u>Condition</u></td>"
+	for bcast_msg in unique_bcasts:
+		exp1_header += "<td width='250px'><u>"+str(bcast_msg)+"</u></td>"
+	exp1_header += "<td width='250px'><u>Overall</u></td>"
+	print(exp1_header)
+	print("</tr>")
+	
+	ratings = []
+	for bcast_msg in unique_bcasts:
+		rating = {}
+		bcast_surveys = thisweeks_bcasts.filter(prompt__file__contains=bcast_msg.message.content_file)
 		
+		rating['verygood_vg'] = Input.objects.filter(call__survey__in=bcast_surveys, call__survey__name__contains='_verygood_', input="1").exclude(call__subject__in=blacklist).distinct().count()
+		rating['verygood_gd'] = Input.objects.filter(call__survey__in=bcast_surveys, call__survey__name__contains='_verygood_', input="2").exclude(call__subject__in=blacklist).distinct().count()
+		rating['verygood_ok'] = Input.objects.filter(call__survey__in=bcast_surveys, call__survey__name__contains='_verygood_', input="3").exclude(call__subject__in=blacklist).distinct().count()
+		
+		rating['goodbad_gd'] = Input.objects.filter(call__survey__in=bcast_surveys, call__survey__name__contains='_goodbad_', input="1").exclude(call__subject__in=blacklist).distinct().count()
+		rating['goodbad_ok'] = Input.objects.filter(call__survey__in=bcast_surveys, call__survey__name__contains='_goodbad_', input="2").exclude(call__subject__in=blacklist).distinct().count()
+		rating['goodbad_bd'] = Input.objects.filter(call__survey__in=bcast_surveys, call__survey__name__contains='_goodbad_', input="3").exclude(call__subject__in=blacklist).distinct().count()
+		
+		ratings.append(rating)
 	
-	# num followups
-	print("<div><h4>Overall</h4></div>")
-	print("<table>")
-
-	exp_start = datetime(year=2010, month=8, day=16)
-	#followups = survey_stats.get_followups(filename=f, date_start=exp_start, quiet=True)
-	followups = survey_stats.get_followups(filename=f, date_start=exp_start, quiet=True, prompt_filter='solution')
-	for source, calllst in followups.items():
-		print("<tr>")
-		print("<td width='40px'>"+str(source)+"</td>")
-		print("<td>"+str(len(calllst))+"</td>")
-		print("</tr>")
+	# overall numbers
+	rating = {}
+	rating['verygood_vg'] = Input.objects.filter(call__survey__in=all_bcasts, call__survey__name__contains='_verygood_', input="1").exclude(call__subject__in=blacklist).distinct().count()
+	rating['verygood_gd'] = Input.objects.filter(call__survey__in=all_bcasts, call__survey__name__contains='_verygood_', input="2").exclude(call__subject__in=blacklist).distinct().count()
+	rating['verygood_ok'] = Input.objects.filter(call__survey__in=all_bcasts, call__survey__name__contains='_verygood_', input="3").exclude(call__subject__in=blacklist).distinct().count()
+	
+	rating['goodbad_gd'] = Input.objects.filter(call__survey__in=all_bcasts, call__survey__name__contains='_goodbad_', input="1").exclude(call__subject__in=blacklist).distinct().count()
+	rating['goodbad_ok'] = Input.objects.filter(call__survey__in=all_bcasts, call__survey__name__contains='_goodbad_', input="2").exclude(call__subject__in=blacklist).distinct().count()
+	rating['goodbad_bd'] = Input.objects.filter(call__survey__in=all_bcasts, call__survey__name__contains='_goodbad_', input="3").exclude(call__subject__in=blacklist).distinct().count()
+	
+	ratings.append(rating)
+	
+	print("<tr>")
+	print("<td>VeryGood</td>")
+	for rating in ratings:
+		print("<td>"+str(rating['verygood_vg'])+" very good; "+str(rating['verygood_gd'])+" good; "+str(rating['verygood_ok'])+" ok</td>")
+	print("</tr>")
+	
+	print("<tr>")
+	print("<td>GoodBad</td>")
+	for rating in ratings:
+		print("<td>"+str(rating['goodbad_gd'])+" good; "+str(rating['goodbad_ok'])+" ok; "+str(rating['goodbad_bd'])+" bad</td>")
+	print("</tr>")
 	
 	print("</table>")
 	
+	if motiv:
+		print("<div><h3>Experiment 2: Self vs. Group Motivation </h4></div>")
+		print_bcast_table(motiv, ['SELF', 'GROUP', 'NONE'])
+		
+	
 	print("</html>")
+
+def print_bcast_table(line, conditions):
+	now = datetime.now()
+	today = datetime(year=now.year, month=now.month, day=now.day)
+	oneday = timedelta(days=1)
+	thisweek = now - timedelta(days=today.weekday())
+	oneweek = timedelta(days=6)
+	
+	print("<div><h4>This week's broadcasts</h4></div>")
+	print("<table>")
+	print("<tr>")
+	
+	thisweeks_bcasts = Survey.objects.filter(broadcast=True, number__in=[line.number, line.outbound_number], call__date__gt=thisweek, call__date__lt=today+oneday).distinct()
+	bcast_prompts = Prompt.objects.filter(survey__in=thisweeks_bcasts, order=3)
+	bcast_prompt_files = [os.path.basename(pair.values()[0]) for pair in bcast_prompts.values('file')]
+	unique_bcasts = Message_forum.objects.filter(message__content_file__in=bcast_prompt_files)
+	
+	all_bcasts = Survey.objects.filter(broadcast=True, number__in=[line.number, line.outbound_number], call__date__gt=STUDY_START, call__date__lt=today+oneday).distinct()
+	
+	exp1_header = "<td width='100px'><u>Condition</u></td>"
+	for bcast_msg in unique_bcasts:
+		exp1_header += "<td width='250px'><u>"+str(bcast_msg)+"</u></td>"
+	exp1_header += "<td width='250px'><u>Overall</u></td>"
+	print(exp1_header)
+	print("</tr>")
+	
+	calls = []
+	for bcast_msg in unique_bcasts:
+		bcast_calls = {}
+		bcast_surveys = thisweeks_bcasts.filter(prompt__file__contains=bcast_msg.message.content_file)
+		
+		for condition in conditions:
+			bcast_calls[condition+'_recipients'] = Call.objects.filter(survey__in=bcast_surveys, survey__name__contains='_'+condition+'_').exclude(subject__in=blacklist).values('subject').distinct().count()
+			bcast_calls[condition+'_completed'] = Call.objects.filter(survey__in=bcast_surveys, survey__name__contains='_'+condition+'_', complete=True).exclude(subject__in=blacklist).values('subject').distinct().count()
+			
+			if condition != 'CALL':
+				bcast_calls[condition+'_behavior'] = Input.objects.filter(call__survey__in=bcast_surveys, call__survey__name__contains='_'+condition+'_').exclude(call__subject__in=blacklist).distinct().count()			
+			else:
+				# only count sessions that have at least one feature access
+				n_one_plus_sessions = 0
+				for survey in bcast_surveys:
+					numbers = Subject.objects.filter(call__survey=survey).exclude(number__in=blacklist_nums).distinct().values('number')
+					numbers = [pair.values()[0] for pair in numbers]
+					calls = Call.objects.filter(survey=survey)
+					firstcalldate = calls.aggregate(Min('date'))
+					firstcalldate = firstcalldate.values()[0]
+					lastcalldate = calls.aggregate(Max('date'))
+					lastcalldate = lastcalldate.values()[0]
+				    
+					calls = num_calls.get_features_within_call(filename=inbound_log, destnum=str(line.number), phone_num_filter=numbers, date_start=firstcalldate, date_end=lastcalldate, quiet=True, transfer_calls=True)
+					feature_calls = calls[calls.keys()[0]] if calls else {}
+					features_hist = {}
+					for call in feature_calls:
+						features_tot = call['q'] + call['a'] + call['r'] + call['e']
+						if features_tot > 0:
+							n_one_plus_sessions += 1
+							
+				bcast_calls[condition+'_behavior'] = n_one_plus_sessions
+				
+		calls.append(bcast_calls)
+	
+	# overall numbers
+	bcast_calls = {}
+	for condition in conditions:
+		bcast_calls[condition+'_recipients'] = 0
+		bcast_calls[condition+'_completed'] = 0
+		bcast_calls[condition+'_behavior'] = 0
+
+	for survey in all_bcasts:
+		for condition in conditions:
+			if '_'+condition+'_' in survey.name:
+				bcast_calls[condition+'_recipients'] += Call.objects.filter(survey__in=survey).exclude(subject__in=blacklist).values('subject').distinct().count()
+				bcast_calls[condition+'_completed'] += Call.objects.filter(survey__in=survey, complete=True).exclude(subject__in=blacklist).values('subject').distinct().count()
+				if condition != 'CALL':
+					bcast_calls[condition+'_behavior'] += Input.objects.filter(call__survey__in=bcast_surveys, call__survey__name__contains='_'+condition+'_').exclude(call__subject__in=blacklist).distinct().count()
+				else:
+					# only count sessions that have at least one feature access
+					n_one_plus_sessions = 0
+					numbers = Subject.objects.filter(call__survey=survey).exclude(number__in=blacklist_nums).distinct().values('number')
+					numbers = [pair.values()[0] for pair in numbers]
+					calls = Call.objects.filter(survey=survey)
+					firstcalldate = calls.aggregate(Min('date'))
+					firstcalldate = firstcalldate.values()[0]
+					lastcalldate = calls.aggregate(Max('date'))
+					lastcalldate = lastcalldate.values()[0]
+				    
+					calls = num_calls.get_features_within_call(filename=inbound_log, destnum=str(line.number), phone_num_filter=numbers, date_start=firstcalldate, date_end=lastcalldate, quiet=True, transfer_calls=True)
+					feature_calls = calls[calls.keys()[0]] if calls else {}
+					features_hist = {}
+					for call in feature_calls:
+						features_tot = call['q'] + call['a'] + call['r'] + call['e']
+						if features_tot > 0:
+							n_one_plus_sessions += 1
+								
+					bcast_calls[condition+'_behavior'] += n_one_plus_sessions
+					
+	calls.append(bcast_calls)
+	
+	for condition in conditions:
+		print("<tr>")
+		print("<td>"+condition+"</td>")
+		for bcast in calls:
+			print("<td>"+str(bcast[condition+'_recipients'])+" of "+str(bcast[condition+'_completed'])+" completed; "+str(bcast_calls[condition+'_behavior'])+" actions</td>")
+		print("</tr>")
+
+	print("</table>")
+	
+	print("<div><h4>Inbound Calling</h4></div>")
+	print("<table>")
+	print("<tr>")
+	print("<td width='100px'><u>Condition</u></td><td width='250px'><u>This week</u></td><td width='250px'><u>Total</u></td>")
+	print("</tr>")
+	
+	for bcast_msg in unique_bcasts:
+		inbound_data = {}
+		bcast_surveys = thisweeks_bcasts.filter(prompt__file__contains=bcast.message.content_file)
+		
+		for condition in conditions:
+			print("<tr>")
+			print("<td>"+condition+"</td>")
+			subjects = Subject.objects.filter(call__survey__in=bcast_surveys, call__survey__name__contains='_'+condition+'_').exclude(number__in=blacklist_nums).distinct()
+			numbers = [subj.number for subj in subjects]
+			calls = num_calls.get_calls(filename=inbound_log, destnum=str(line.number), phone_num_filter=numbers, date_start=thisweek, quiet=True)
+			n_thisweek = calls[calls.keys()[0]] if calls else 0
+			posts = Message_forum.objects.filter(message__date__gte=thisweek, message__date__lt=today+oneday, forum__line=bang, message__number__in=call_numbers)
+			n_approved = posts.filter(status = Message_forum.STATUS_APPROVED).count()
+			print("<td>"+str(n_thisweek)+" calls; "+str(posts.count())+" posts ("+str(n_approved)+" approved)</td>")
+				
+			calls = num_calls.get_calls(filename=inbound_log, destnum=str(line.number), phone_num_filter=numbers, date_start=STUDY_START, quiet=True)
+			n_total = calls[calls.keys()[0]] if calls else 0
+			posts = Message_forum.objects.filter(message__date__gte=STUDY_START, message__date__lt=today+oneday, forum__line=bang, message__number__in=call_numbers)
+			n_approved = posts.filter(status = Message_forum.STATUS_APPROVED).count()
+			print("<td>"+str(n_total)+" calls; "+str(posts.count())+" posts ("+str(n_approved)+" approved)</td>")
+
+
+			print("</tr>")
+			
+	print("</table>")
 
 def main():
 	if len(sys.argv) < 2:
 		print("Wrong")
 	else:
 		f = sys.argv[1]
-		if len(sys.argv) > 2:
-			datestr = sys.argv[2]
-			date = datetime.strptime(datestr, '%m-%d-%Y')
-		else:
-			now = datetime.now()
-			date = datetime(year=now.year, month=now.month, day=now.day)
-	print_digest(f, date)
+		bangid = sys.argv[2]
+		bang = Line.objects.get(pk=int(bangid))	
+		motiv = None
+		if len(sys.argv) > 3:
+			motivid = sys.argv[3]
+			motiv = Line.objects.get(pk=int(motivid))
+	
+	print_digest(f, bang, motiv)
     
 main()
