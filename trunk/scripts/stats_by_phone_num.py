94c0a13f5f23fa959d3922da7f63a4804afcfe35
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2011-05-17 01:12:10 +0000

    add thesis funcs

--- trunk/scripts/stats_by_phone_num.py
+++ trunk/scripts/stats_by_phone_num.py
@@ -3,9 +3,13 @@ import sys
 from datetime import datetime
 from otalo.AO.models import Line, Forum, User, Message
 
-def get_calls_by_number(filename, destnum=False, log="Start call", date_start=False, date_end=False, quiet=False, legacy_log=False, transfer_calls=False):
+def get_calls_by_number(filename, destnum=False, log="Start call", phone_num_filter=False, date_start=False, date_end=False, quiet=False, legacy_log=False, transfer_calls=False):
 	calls = {}
 	phone_nums = ''
+	
+	if phone_num_filter:
+		for num in phone_num_filter:
+			calls[num] = 0
 	
 	f = open(filename)
 
@@ -25,6 +29,10 @@ def get_calls_by_number(filename, destnum=False, log="Start call", date_start=Fa
 			dest = otalo_utils.get_destination(line, legacy_log)		
 		##
 		################################################
+		
+			if phone_num_filter and not phone_num in phone_num_filter:
+				continue
+			
 			if date_start:
 				if date_end:
 					if not (current_date >= date_start and current_date < date_end):
@@ -71,9 +79,10 @@ def get_calls_by_number(filename, destnum=False, log="Start call", date_start=Fa
 		print("Number of "+ log + "'s by phone number:")
 		
 		total = 0
-		for num, tot in calls_sorted:
+		#for num, tot in calls_sorted:
+		for num, tot in calls.items():
 			total += tot
-			print(num +": "+str(tot))
+			print(num +"\t"+str(tot))
 			
 		print('total is ' + str(total));	
 		print('numbers are ' + phone_nums)
@@ -447,7 +456,233 @@ def get_posts_by_caller(line, forums=None, date_start=False, date_end=False, qui
 		print('total is ' + str(total));	
 		print('num callers is ' + str(n_callers));	
 	return calls_sorted
+
+def get_online_time(filename, destnum=False, phone_num_filter=False, date_start=False, date_end=False, quiet=False):
+	online_time = {}
+	current_day = 0
+	open_calls = {}
+	
+	f = open(filename)
+	
+	if phone_num_filter:
+		# every phone number has an entry
+		for num in phone_num_filter:
+			online_time[num] = 0
+	
+	while(True):
+		line = f.readline()
+		if not line:
+			break
+		try:
 		
+		#################################################
+		## Use the calls here to determine what pieces
+		## of data must exist for the line to be valid.
+		## All of those below should probably always be.
+		
+			phone_num = otalo_utils.get_phone_num(line)
+			current_date = otalo_utils.get_date(line)	
+			dest = otalo_utils.get_destination(line)			
+		##
+		################################################
+			
+			current_time = otalo_utils.get_time(line)
+			
+			if phone_num_filter and not phone_num in phone_num_filter:
+				continue
+				
+			if date_start:
+				if date_end:
+					if not (current_date >= date_start and current_date < date_end):
+						continue
+					if current_date > date_end:
+						break
+				else:
+					if not current_date >= date_start:
+						continue
+			
+			if destnum and destnum.find(dest) == -1:
+				continue
+			
+			if line.find("Start call") != -1:
+				# check to see if this caller already has one open
+				if phone_num in open_calls.keys() and current_time > open_calls[phone_num]['last']:
+					# close out current call					
+					call = open_calls[phone_num]
+					del open_calls[phone_num]
+					dur = call['last'] - call['start']		
+					#print("closing out call pre-emptively: " + phone_num + ", "+otalo_utils.date_str(current_date) + ", "+otalo_utils.get_sessid(line) + ", duration: " + str(dur.seconds))
+					if phone_num in online_time:
+						online_time[phone_num] += dur.seconds
+					else:
+						online_time[phone_num] = dur.seconds
+				# add new call
+				#print("adding new call: " + phone_num)
+				open_calls[phone_num] = {'start':current_time, 'last':current_time }
+					
+			elif line.find("End call") != -1:
+				if phone_num in open_calls.keys():
+					# close out call				
+					call = open_calls[phone_num]
+					dur = current_time - call['start']			
+					#print("closing out call: "+phone_num + ", "+otalo_utils.date_str(current_date) + ", "+ otalo_utils.get_sessid(line) + ", duration: " + str(dur.seconds))
+					if phone_num in online_time:
+						online_time[phone_num] += dur.seconds
+					else:
+						online_time[phone_num] = dur.seconds
+					del open_calls[phone_num]
+			elif phone_num in open_calls:
+				#print("updating call dur: " + phone_num)
+				# this makes things conservative. A call is only officially counted if
+				# it starts with a call_started
+				open_calls[phone_num]['last'] = current_time
+			
+			#print("open_calls: " + str(open_calls))
+		
+		except KeyError as err:
+			#print("KeyError: " + phone_num + "-" + otalo.date_str(current_date) + " " + otalo.time_str(current_time))
+			raise
+		except ValueError as err:
+			#print("ValueError: " + line)
+			continue
+		except IndexError:
+			continue
+		except otalo_utils.PhoneNumException:
+			#print("PhoneNumException: " + line)
+			continue
+	
+	#flush the last week
+	#flush_open_calls(online_time, open_calls, current_day)
+	if not quiet:
+		print("Total online time, by phone number (s):")
+		for num, tot in online_time.items():
+			print(num +"\t"+str(tot))
+			
+	return online_time
+
+def get_num_listens(filename, destnum, phone_num_filter=False, date_start=False, date_end=False, quiet=False, transfer_calls=False):
+	listens = {}
+	durations = {}
+	current_week_start = 0
+	open_calls = {}
+	
+	if phone_num_filter:
+		# every phone number has an entry
+		for num in phone_num_filter:
+			listens[num] = 0
+	
+	f = open(filename)
+	
+	while(True):
+		line = f.readline()
+		if not line:
+			break
+		try:
+		
+		#################################################
+		## Use the calls here to determine what pieces
+		## of data must exist for the line to be valid.
+		## All of those below should probably always be.
+		
+			phone_num = otalo_utils.get_phone_num(line)
+			current_date = otalo_utils.get_date(line)		
+			dest = otalo_utils.get_destination(line)			
+		##
+		################################################
+			
+			if phone_num_filter and not phone_num in phone_num_filter:
+				continue
+				
+			if date_start:
+				if date_end:
+					if not (current_date >= date_start and current_date < date_end):
+						continue
+					if current_date > date_end:
+						break
+				else:
+					if not current_date >= date_start:
+						continue
+			
+			if destnum.find(dest) == -1:
+				continue
+			
+			# A hacky way to test for transfer call
+			# In the future you want to compare this call's
+			# start time to a time window related to the end
+			# of the survey call (in which you can keep the flag
+			# false and give a more targeted start and end date)
+			if transfer_calls:
+				if len(dest) < 10:
+					continue
+			elif len(dest) == 10:
+				continue
+			
+			if line.find("Start call") != -1:
+				# check to see if this caller already has one open
+				if phone_num in open_calls:
+					# close out current call
+					call = open_calls[phone_num]
+					n_listens = call['listens']
+					if phone_num in listens:
+						listens[phone_num] += n_listens
+					else:
+						listens[phone_num] = n_listens
+					dur = call['last'] - call['start']
+					if n_listens in durations:
+						durations[n_listens].append(dur)
+					else:
+						durations[n_listens] = [dur]
+					del open_calls[phone_num]
+				open_calls[phone_num] = {'start':current_date, 'last':current_date, 'listens':0 }
+				
+			elif line.find("End call") != -1:
+				if phone_num in open_calls:
+					# close out call				
+					call = open_calls[phone_num]
+					n_listens = call['listens']
+					if phone_num in listens:
+						listens[phone_num] += n_listens
+					else:
+						listens[phone_num] = n_listens
+					dur = current_date - call['start']
+					if n_listens in durations:
+						durations[n_listens].append(dur)
+					else:
+						durations[n_listens] = [dur]
+					del open_calls[phone_num]
+			else:
+				if line.find("Stream") != -1:
+					if phone_num in open_calls:
+						open_calls[phone_num]['listens'] += 1
+				if phone_num in open_calls:
+					#print("updating call dur: " + phone_num)
+					# this makes things conservative. A call is only officially counted if
+					# it starts with a call_started
+					open_calls[phone_num]['last'] = current_date
+				
+			
+		except KeyError as err:
+			#print("KeyError: " + phone_num + "-" + otalo.date_str(current_date))
+			raise
+		except ValueError as err:
+			#print("ValueError: " + line)
+			continue
+		except IndexError as err:
+			#print("IndexError: " + line)
+			continue
+		except otalo_utils.PhoneNumException:
+			#print("PhoneNumException: " + line)
+			continue
+	
+	if not quiet:
+		if phone_num_filter:
+			print("Data for phone numbers: " + str(phone_num_filter))
+				
+		print("Number of listens, by number:")
+		for num, tot in listens.items():			
+			print(str(num) + "\t" + str(tot))	
+			
+	return listens
 
 def main():
 	if not len(sys.argv) > 1:
@@ -458,15 +693,16 @@ def main():
 		if len(sys.argv) == 3:
 			lineid = sys.argv[2]
 			line = Line.objects.get(pk=lineid)
-			print('num is ' + line.number)
+			#print('num is ' + line.number)
 		if len(sys.argv) == 4:
 			demographics_file = sys.argv[3]
 		
-		#get_calls_by_number(f,line.number)
+		start=datetime(year=2010,month=9,day=30)
+		get_calls_by_number(f,line.number, date_start=start)
 		#get_guj_nums_only(f, legacy_log=True)
 		#get_calls_by_feature(f)
 		#get_calls_by_geography(f, demographics_file)
 		#get_messages_by_number(['9586550654'])
-		get_numbers_by_date(f, line.number)
+		#get_numbers_by_date(f, line.number)
 			
 #main()
