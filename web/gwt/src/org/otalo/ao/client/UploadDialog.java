44edef2d3822ee279b3ac3cc2f263be511e02e45
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2011-05-24 20:33:23 +0000

    reverted to 575

--- web/gwt/src/org/otalo/ao/client/UploadDialog.java
+++ web/gwt/src/org/otalo/ao/client/UploadDialog.java
@@ -41,127 +41,126 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 
 
-
 public class UploadDialog extends DialogBox {
-        private FormPanel uploadForm = new FormPanel();
-        private Hidden forumId = new Hidden("forumid");
-        private Hidden messageForumId = new Hidden("messageforumid");
-        private TextBox number;
-        private HorizontalPanel numberPanel = new HorizontalPanel();
-        private HorizontalPanel contentPanel = new HorizontalPanel();
-        public static final int INVALID_NUMBER = 1;
-        public static final int NO_CONTENT = 2;
-        
-        public UploadDialog() {
-                setText("Upload Content");
-                FlexTable outer = new FlexTable();
-                outer.setSize("100%", "100%");
-                uploadForm.setAction(JSONRequest.BASE_URL+AoAPI.UPLOAD);
-                uploadForm.setMethod(FormPanel.METHOD_POST);
-                uploadForm.setEncoding(FormPanel.ENCODING_MULTIPART);
-                
-                FileUpload main = new FileUpload();
-                main.setName("main");
-                main.setTitle("Content");
-                Label mainLabel = new Label("Content:");
-                
-                FileUpload summary = new FileUpload();
-                summary.setName("summary");
-                summary.setTitle("Summary");
-                Label summaryLabel = new Label("Summary (optional):");
-                
-                number = new TextBox();
-                number.setName("number");
-                User moderator = Messages.get().getModerator();
-                if (moderator != null)
-                        // default is the moderator's number
-                        number.setValue(moderator.getNumber());
-                Label numberLabel = new Label("Author Number:");
-                
-                Button saveButton = new Button("Save", new ClickHandler() {
+	private FormPanel uploadForm = new FormPanel();
+	private Hidden forumId = new Hidden("forumid");
+	private Hidden messageForumId = new Hidden("messageforumid");
+	private TextBox number;
+	private HorizontalPanel numberPanel = new HorizontalPanel();
+	private HorizontalPanel contentPanel = new HorizontalPanel();
+	public static final int INVALID_NUMBER = 1;
+	public static final int NO_CONTENT = 2;
+	
+	public UploadDialog() {
+		setText("Upload Content");
+		FlexTable outer = new FlexTable();
+		outer.setSize("100%", "100%");
+		uploadForm.setAction(JSONRequest.BASE_URL+AoAPI.UPLOAD);
+		uploadForm.setMethod(FormPanel.METHOD_POST);
+		uploadForm.setEncoding(FormPanel.ENCODING_MULTIPART);
+		
+		FileUpload main = new FileUpload();
+		main.setName("main");
+		main.setTitle("Content");
+		Label mainLabel = new Label("Content:");
+		
+		FileUpload summary = new FileUpload();
+		summary.setName("summary");
+		summary.setTitle("Summary");
+		Label summaryLabel = new Label("Summary (optional):");
+		
+		number = new TextBox();
+		number.setName("number");
+		User moderator = Messages.get().getModerator();
+		if (moderator != null)
+			// default is the moderator's number
+			number.setValue(moderator.getNumber());
+		Label numberLabel = new Label("Author Number:");
+		
+		Button saveButton = new Button("Save", new ClickHandler() {
       public void onClick(ClickEvent event) {
-        uploadForm.submit();
+      	uploadForm.submit();
       }
     });
-                
-                Button cancelButton = new Button("Cancel", new ClickHandler() {
+		
+		Button cancelButton = new Button("Cancel", new ClickHandler() {
       public void onClick(ClickEvent event) {
-        hide();
+      	hide();
       }
     });
-                
-                outer.setWidget(0, 0, mainLabel);
-                outer.getCellFormatter().setWordWrap(0, 0, false);
-                contentPanel.setSpacing(2);
-                DOM.setStyleAttribute(contentPanel.getElement(), "textAlign", "left");
-                contentPanel.add(main);
-                outer.setWidget(0, 1, contentPanel);
-                outer.setWidget(1, 0, summaryLabel);
-                outer.getCellFormatter().setWordWrap(1, 0, false);
-                outer.setWidget(1, 1, summary);
-                outer.setWidget(2, 0, numberLabel);
-                outer.getCellFormatter().setWordWrap(2, 0, false);
-                numberPanel.setSpacing(2);      
-                DOM.setStyleAttribute(numberPanel.getElement(), "textAlign", "left");
-                numberPanel.add(number);
-                outer.setWidget(2, 1, numberPanel);
+		
+		outer.setWidget(0, 0, mainLabel);
+		outer.getCellFormatter().setWordWrap(0, 0, false);
+		contentPanel.setSpacing(2);
+		DOM.setStyleAttribute(contentPanel.getElement(), "textAlign", "left");
+		contentPanel.add(main);
+		outer.setWidget(0, 1, contentPanel);
+		outer.setWidget(1, 0, summaryLabel);
+		outer.getCellFormatter().setWordWrap(1, 0, false);
+		outer.setWidget(1, 1, summary);
+		outer.setWidget(2, 0, numberLabel);
+		outer.getCellFormatter().setWordWrap(2, 0, false);
+		numberPanel.setSpacing(2);	
+		DOM.setStyleAttribute(numberPanel.getElement(), "textAlign", "left");
+		numberPanel.add(number);
+		outer.setWidget(2, 1, numberPanel);
 
-                
-                HorizontalPanel buttons = new HorizontalPanel();
-                // tables don't obey the setHorizontal of parents, and buttons is a table,
-                // so use float instead
-                DOM.setStyleAttribute(buttons.getElement(), "cssFloat", "right");
-                buttons.add(saveButton);
-                buttons.add(cancelButton);
-                outer.setWidget(3, 1, buttons);
-                
-                outer.setWidget(outer.getRowCount(), 0, forumId);
-                outer.setWidget(outer.getRowCount(), 0, messageForumId);
-                
-                uploadForm.setWidget(outer);
-                
-                setWidget(uploadForm);
-        }
-        
-        public void setForum(Forum f)
-        {
-                forumId.setValue(f.getId());
-        }
-        
-        public void setMessageForum(MessageForum mf)
-        {
-                messageForumId.setValue(mf.getId());
-        }
-        
-        public void validationError(int type, String msg)
-        {
-                HTML msgHTML = new HTML("<span style='color:red'>("+msg+")</span>");
-                if (type == INVALID_NUMBER && numberPanel.getWidgetCount() == 1)
-                {
-                        numberPanel.insert(msgHTML, 0);
-                }
-                else if (type == NO_CONTENT && contentPanel.getWidgetCount() == 1)
-                {
-                        contentPanel.insert(msgHTML, 0);
-                }
-        }
-        
-        public void reset()
-        {
-                uploadForm.reset();
-                User moderator = Messages.get().getModerator();
-                if (numberPanel.getWidgetCount() == 2)
-                        numberPanel.remove(0);
-                if (contentPanel.getWidgetCount() == 2)
-                        contentPanel.remove(0);
-                if (moderator != null)
-                        // default is the moderator's number
-                        number.setValue(moderator.getNumber());
-        }
-        
-        public void setCompleteHandler(SubmitCompleteHandler handler)
-        {
-                uploadForm.addSubmitCompleteHandler(handler);
-        }
+		
+		HorizontalPanel buttons = new HorizontalPanel();
+		// tables don't obey the setHorizontal of parents, and buttons is a table,
+		// so use float instead
+		DOM.setStyleAttribute(buttons.getElement(), "cssFloat", "right");
+		buttons.add(saveButton);
+		buttons.add(cancelButton);
+		outer.setWidget(3, 1, buttons);
+		
+		outer.setWidget(outer.getRowCount(), 0, forumId);
+		outer.setWidget(outer.getRowCount(), 0, messageForumId);
+		
+		uploadForm.setWidget(outer);
+		
+		setWidget(uploadForm);
+	}
+	
+	public void setForum(Forum f)
+	{
+		forumId.setValue(f.getId());
+	}
+	
+	public void setMessageForum(MessageForum mf)
+	{
+		messageForumId.setValue(mf.getId());
+	}
+	
+	public void validationError(int type, String msg)
+	{
+		HTML msgHTML = new HTML("<span style='color:red'>("+msg+")</span>");
+		if (type == INVALID_NUMBER && numberPanel.getWidgetCount() == 1)
+		{
+			numberPanel.insert(msgHTML, 0);
+		}
+		else if (type == NO_CONTENT && contentPanel.getWidgetCount() == 1)
+		{
+			contentPanel.insert(msgHTML, 0);
+		}
+	}
+	
+	public void reset()
+	{
+		uploadForm.reset();
+		User moderator = Messages.get().getModerator();
+		if (numberPanel.getWidgetCount() == 2)
+			numberPanel.remove(0);
+		if (contentPanel.getWidgetCount() == 2)
+			contentPanel.remove(0);
+		if (moderator != null)
+			// default is the moderator's number
+			number.setValue(moderator.getNumber());
+	}
+	
+	public void setCompleteHandler(SubmitCompleteHandler handler)
+	{
+		uploadForm.addSubmitCompleteHandler(handler);
+	}
 
 }
