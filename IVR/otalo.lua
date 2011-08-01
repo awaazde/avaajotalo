3e2ea645f1f3015a30404c842f3882f4f11098c1
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2011-08-01 02:28:07 +0000

    should order threads by lft, not date since uploaded responses could come in out of date order

--- IVR/otalo.lua
+++ IVR/otalo.lua
@@ -203,10 +203,10 @@ end
 
 function getreplies (thread)
    local query = "SELECT message.id, message.content_file, message.summary_file, message.rgt, message.thread_id, forum.id, forum.responses_allowed, forum.moderated, message_forum.status, forum.confirm_recordings, forum.maxlength ";
-   query = query .. "FROM AO_message message, AO_message_forum message_forum, AO_forum forum ";
-   query = query .. "WHERE message.thread_id = " .. thread .. " AND message_forum.message_id = message.id ";
+   query = query .. " FROM AO_message message, AO_message_forum message_forum, AO_forum forum ";
+   query = query .. " WHERE message.thread_id = " .. thread .. " AND message_forum.message_id = message.id ";
    query = query .. " AND message_forum.status = " .. MESSAGE_STATUS_APPROVED .. " AND message_forum.forum_id = forum.id ";
-   query = query .. "ORDER BY message.lft";
+   query = query .. " ORDER BY message.lft";
    freeswitch.consoleLog("info", script_name .. " : query : " .. query .. "\n");
    return rows(query);
 end
