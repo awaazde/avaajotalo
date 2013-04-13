371987046fd3cbb5d8dff718cd4736cbcf2deeac
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2013-04-13 09:45:41 +0000

    logout's log back in link points to previous (referring) app

---
+++ trunk/web/django/views.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.contrib.auth import logout
+from django.shortcuts import render
+
+'''
+'    Django is not happy if you overload
+'    the name 'logout' for a view
+'''
+def log_out(request):
+    logout(request)
+    referrer = request.META['HTTP_REFERER']
+    referrer = referrer[referrer.find(settings.CONSOLE_ROOT):]
+    return render(request, 'registration/logged_out.html', {'referrer':referrer})
+    
