0e2a15b6ddc756f2a5c50132667204029507e7cf
nikhil.navadiya@gmail.com <nikhil.navadiya@gmail.com@1acfb158-7363-11de-834f-c7280bcb9d8a>
2013-09-11 05:01:52 +0000

    Committing Tag refactoring code changes into trunk. Reference Asana Task# https://app.asana.com/0/7274313200630/7604169259895

--- trunk/web/gwt/src/org/otalo/ao/client/AOTagWidget.java
+++ trunk/web/gwt/src/org/otalo/ao/client/AOTagWidget.java
@@ -159,5 +159,8 @@ public class AOTagWidget extends TagWidget {
 		tagsChanged.setValue("0");
 		 
 	}
-
+	
+	public void setSelectedTagData() {
+		//nothing to do here
+	}
 }
--- trunk/web/gwt/src/org/otalo/ao/client/MessageDetail.java
+++ trunk/web/gwt/src/org/otalo/ao/client/MessageDetail.java
@@ -88,7 +88,7 @@ public class MessageDetail extends Composite {
   	detailsTable = new FlexTable();
   	metadata = new VerticalPanel();
   	metadata.setHeight("100%");
-  	tags = new AOTagWidget();
+  	tags = new AutoCompleteTagWidget();
   	routing = new AORoutingWidget();
   	metadata.setVerticalAlignment(HasVerticalAlignment.ALIGN_TOP);
   	metadata.add(tags);
@@ -145,6 +145,8 @@ public class MessageDetail extends Composite {
   	
   	saveButton = new Button("Save", new ClickHandler() {
       public void onClick(ClickEvent event) {
+    	//calling setSelectedTagData so that selected tags can be collected and set as value to selectedTags input.
+    	tags.setSelectedTagData();
       	setClickedButton(saveButton);
       	detailsForm.setAction(JSONRequest.BASE_URL + AoAPI.UPDATE_MESSAGE);
         detailsForm.submit();
