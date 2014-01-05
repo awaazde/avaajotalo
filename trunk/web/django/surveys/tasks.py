a4be62d63f002c519ab1e38084977994f74db09c
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2014-01-05 06:41:42 +0000

    ao beat tasks: accept positional (variable) args; add back congestion control for surveytasks.schedule_call

--- trunk/web/django/surveys/tasks.py
+++ trunk/web/django/surveys/tasks.py
@@ -13,7 +13,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #===============================================================================
-import time
+import time, re
 from datetime import datetime
 from celery.exceptions import MaxRetriesExceededError
 from celery import shared_task
