d6831f835b5663427f89c4c9b72e27b52d26f82d
neilpatel <neilpatel@1acfb158-7363-11de-834f-c7280bcb9d8a>
2014-07-22 07:36:47 +0000

    bug fix audio conversion race condition with remote syncing of files. Make sure conversion will only start after files have completely finished converting.

--- trunk/web/django/utils/audio/audio_converter.py
+++ trunk/web/django/utils/audio/audio_converter.py
@@ -19,6 +19,28 @@ import sys, os, time, subprocess
 from datetime import datetime, timedelta
 from ao.models import Message 
 
+'''
+Allow some buffer of time to look for audio to convert,
+in order to avoid race conditions with remote syncing of files.
+
+This is needed in a case where
+1. A file is recorded on a remote server
+2. The remote server executes an rsync within X min (where X is how often the cron runs)
+3. In X + epsilon, the audio converter spots message, sees a wav file that is partially
+uploaded, but begins conversion anyway.
+
+We need to make sure conversion only happens after sync is complete.
+The best way is to taskify syncing and make conversion happen after that, but
+this is a temporary solution. The downside to this approach is there will
+be further delay for a mp3 file to be created, hence playback in the console
+will be further delayed.
+
+The shortest you can make this BUFFER is how often the sync crons run.
+If you make it longer, it is being more conservative, but it can't be any
+shorter
+'''
+BUFFER_MINS = 2
+
 def convert_to_mp3(file_path):
     #converting audio file to mp3
     if ".wav" in file_path:
@@ -28,9 +50,10 @@ def convert_to_mp3(file_path):
         print "converted %s"%(to_path)
     
 def main(interval_mins):
-    interval = datetime.now() - timedelta(minutes=interval_mins)
+    now = datetime.now()
+    interval_start = now - timedelta(minutes=BUFFER_MINS) - timedelta(minutes=interval_mins)
     #iterating thorough all the message objects
-    for m in Message.objects.filter(date__gte=interval):
+    for m in Message.objects.filter(date__gte=interval_start, date__lt=now-timedelta(minutes=BUFFER_MINS)):
         print('found message '+str(m) + ' file is '+ m.file.path)
         if '.wav' in m.file.path:
             mp3_file_path  = m.file.path[:m.file.path.rfind(".wav")] + ".mp3"
