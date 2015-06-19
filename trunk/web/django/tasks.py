895b7b4e84d2740c34e182a4956791c13c3b33ff
nikhil.navadiya@gmail.com <nikhil.navadiya@gmail.com@1acfb158-7363-11de-834f-c7280bcb9d8a>
2015-06-19 17:06:57 +0000

    media sync changes

---
+++ trunk/web/django/tasks.py
@@ -0,0 +1,46 @@
+#===============================================================================
+#    Copyright (c) 2015 Awaaz.De Infosystems Pvt Ltd
+# 
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+# 
+#        http://www.apache.org/licenses/LICENSE-2.0
+# 
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#===============================================================================
+import re
+from datetime import datetime, timedelta
+from celery import shared_task
+from django.conf import settings
+from otalo.utils import audio_utils
+from otalo.ao.models import Dialer
+from otalo.surveys.models import Prompt
+
+
+'''
+' Sync the survey prompt audio on the give machine - we need it because of survey task - before creating call, 
+' we are calling this method to check if all survey files are present
+'''
+@shared_task
+def sync_survey_audio(s, machine_id):
+    is_cached = True
+    for p in Prompt.objects.filter(survey=s):
+        is_cached = is_cached and audio_utils.cache_audio(str(p.file))
+    
+    return is_cached
+
+
+'''
+' Sync audio file from master server in the slave machine
+' file parameter is a full file path e.g. /home/awaazde/media/forum/sefc_hindi/listen.wav or stream file
+' like /home/awaazde/media/2015/01/01/2015_02922323.wav
+'''
+@shared_task
+def sync_audio_file(file, machine_id):
+    audio_utils.cache_audio(file)
+
---
+++ trunk/web/django/utils/sync_utils.py
@@ -0,0 +1,57 @@
+import os
+from os import listdir
+from os.path import isfile, join
+from otalo.ao.models import Line, Dialer
+from otalo import tasks
+
+
+'''
+' Sync audio file from master server in the slave machine
+' file parameter is a full file path e.g. /home/awaazde/media/forum/sefc_hindi/listen.wav or stream file
+' like /home/awaazde/media/2015/01/01/2015_02922323.wav
+'''
+def sync_file(file, dialers):
+    machine_ids = get_unique_machines(dialers)
+    for mid in machine_ids:
+        tasks.sync_audio_file.s().delay(file, mid)
+        
+        
+
+'''
+' Sync all files from given folder from master server in the slave machine
+'
+'''
+def sync_folder(filedir, dialers):
+    machine_ids = get_unique_machines(dialers)
+    #getting files
+    files = [ join(filedir,f) for f in listdir(filedir) if isfile(join(filedir,f)) and os.path.splitext(join(filedir,f))[1] != '.zip' ]   
+    
+    
+    #running task on every machine for every file 
+    for file in files:
+        for mid in machine_ids:
+            tasks.sync_audio_file.s().delay(file, mid)
+
+
+
+'''
+' Sync all files from given survey from master server in the slave machine
+'
+'''
+def sync_survey_files(survey):
+    machine_ids = get_unique_machines(survey.dialers.all())
+    
+    for mid in machine_ids:
+        tasks.sync_survey_audio.s().delay(survey, mid)
+
+'''
+This method returns machines from dialers 
+'''
+def get_unique_machines(dialers):
+    machine_ids = []
+    
+    for d in dialers:
+        machine_ids.append(d.machine_id[0])
+        machine_ids = list(set(machine_ids))
+    
+    return machine_ids
